5cf88b401b38cf1756df73ddf0424826bfcb3c62
Pedro Martin <pataruco@gmail.com>
2015-05-25 18:52:30 +0100

    17th question answered

--- raw.sql
+++ raw.sql
@@ -30,6 +30,6 @@
 
 -- 16 SELECT products.name, COUNT(product_id) AS noitems FROM wishlists inner join products on products.id = wishlists.product_id group by product_id order by noitems desc;
 
-17. Inserts a user with the name "Jonathan Anderson" into the users table. Ensure the created_at column is set to the current time.
+-- 18 INSERT INTO users (name, created_at) VALUES ("Jonathan Anderson", CURRENT_TIMESTAMP);
 
-
+18. Selects the id of the user with the name "Jonathan Anderson"?
